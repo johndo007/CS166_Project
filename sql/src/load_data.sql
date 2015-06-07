53a26a695445fc860cd60369b442369098e6f473
johndo007 <jdo007@ucr.edu>
2015-06-06 17:17:22 -0700

    Updated Load file

--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -4,5 +4,44 @@ COPY USR(
 	email,
 	name,
 	dateOfBirth)
-FROM '/home/csmajs/jdo007/CS166_Project/data/User_Data'
+FROM '/home/csmajs/jdo007/CS166_Project/data/USR'
 WITH DELIMITER ';';
+
+COPY WORK_EXPR(
+	userId,
+	company,
+	role,	
+	location,
+	startDate,
+	endDate)
+FROM '/home/csmajs/jdo007/CS166_Project/data/WORK_EX'
+WITH DELIMITER ';';
+
+COPY EDUCATIONAL_DETAILS(
+	userId,
+	instituitionName,
+	major,
+	degree,
+	startDate,
+	endDate)
+FROM '/home/csmajs/jdo007/CS166_Project/data/EDU_DET'
+WITH DELIMITER ';';
+
+COPY CONNECTION_USR(
+        userId,
+        connectionId,
+        status)
+FROM '/home/csmajs/jdo007/CS166_Project/data/CONNECTION'
+WITH DELIMITER ';';
+
+COPY MESSAGE(
+        msgId,
+        senderId,
+        receiverId,
+        contents, 
+        sendTime,
+        deleteStatus,
+        status)
+FROM '/home/csmajs/jdo007/CS166_Project/data/MESSAGE'
+WITH DELIMITER ';';
+
